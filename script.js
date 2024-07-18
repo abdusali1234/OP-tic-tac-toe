7c57a3d2783950118d7080598766a73f5a7b6d37
abdusali1234 <abdusali1234@gmail.com>
2024-07-18 07:26:22 +0100

    added factory function for gameboard

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log('Tic tac toe!');
+
+function gameBoard() {
+
+    let board = Array(9).fill(null);
+
+    const getBoard = () => {
+        board;
+    }
+
+    function displayBoard() {
+        console.log(`
+            ${board[0]} | ${board[1]} | ${board[2]}
+            --+---+--
+            ${board[3]} | ${board[4]} | ${board[5]}
+            --+---+--
+            ${board[6]} | ${board[7]} | ${board[8]}
+            `)
+    };
+
+    function setCell(index, marker){
+        if (board[index] == null){
+            board[index] = marker;
+            return true;
+        }
+        return false;
+    };
+
+    const clearBoard = () => {
+        board.fill(null);
+    }
+
+    return {getBoard, displayBoard, setCell, clearBoard}
+}
+
+gameBoard().displayBoard()
