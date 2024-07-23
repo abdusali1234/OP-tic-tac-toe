26bbcb36c9e1eac5714b56e9154969e6bdccaf9b
abdusali1234 <abdusali1234@gmail.com>
2024-07-23 11:12:53 +0100

    fixed bugs by starting from scratch :p

--- script.js
+++ script.js
@@ -1,50 +1,107 @@
-console.log('Tic tac toe!');
-
-const gameBoard= ( () => {
-
-    let board = Array(9).fill(null);
-
-
-
-    const getBoard = () => board;
-
-    const displayBoard = () => {
-        console.log(`
-            ${board[0]} | ${board[1]} | ${board[2]}
-            --+---+--
-            ${board[3]} | ${board[4]} | ${board[5]}
-            --+---+--
-            ${board[6]} | ${board[7]} | ${board[8]}
-            `)
-    };
-
-    function setCell(index, marker){  
-        if (board[index] == null){
-            board[index] = marker;
-            validCell = true;
-            return true;
-        }
-        return false;
-    };
-
-
-    const clearBoard = () => {
-        board.fill(null);
-    };
-
-    return {getBoard, displayBoard, setCell, clearBoard}
+const displayController = (() => {
+    const renderMessage = (message) => {
+        document.getElementById("message").innerHTML = message;
+        
+    }
+    return {renderMessage}
 })();
 
-function createPlayer(name, marker){
-    return {name, marker};
+const gameBoard = (() => {
+    let board = Array(9).fill("");
+    let gameBoard = document.getElementById("gameboard");
+
+    const renderBoard = () => {
+        gameBoard.innerHTML = "";
+        
+        board.forEach((cell, index) => {
+            const cellEl = document.createElement('div');
+            cellEl.classList.add('cell');
+            cellEl.setAttribute("id", `cell-${index}`)
+            cellEl.innerHTML = `${cell}`;
+            gameBoard.appendChild(cellEl);
+        });
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => {
+            cell.addEventListener("click", gameController.handleClick);
+        });    
+    };
+
+    const updateBoard = (index, value) => {
+        board[index] = value;
+        renderBoard();
+    }
+
+    const getGameBoard = () => board;
+    return { renderBoard, updateBoard, getGameBoard}
+})();
+
+const createPlayer = (name, marker) => {
+    return {name, marker}
 };
 
-const game = (function() {
-    let playerOne;
-    let playerTwo;
-    let currentPlayer;
-    let isGameOver;
+const gameController = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
 
+    const start = () => {
+        players = [
+            createPlayer(document.getElementById("player1").value, "X"),
+            createPlayer(document.getElementById("player2").value, "O")
+        ];
+        currentPlayerIndex = 0;
+        let gameOver = false;
+        gameBoard.renderBoard();
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => {
+            cell.addEventListener("click", handleClick);
+        });
+    };
+
+    const switchPlayers = () => {
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    }
+
+    
+
+    const handleClick = (event) => {
+        if (gameOver){
+            return;
+        }
+        let index = parseInt(event.target.id.split("-")[1]);
+
+        if (gameBoard.getGameBoard()[index] !== ""){
+            return;
+        } 
+        gameBoard.updateBoard(index, players[currentPlayerIndex].marker);
+
+        if (checkForWin(gameBoard.getGameBoard(), players[currentPlayerIndex].marker)) {
+            gameOver = true;
+            displayController.renderMessage(`${players[currentPlayerIndex].name} wins!`)
+        } else if (checkForTie(gameBoard.getGameBoard())) {
+            gameOver = true;
+            displayController.renderMessage("It's a tie!");
+
+        }
+
+        switchPlayers();
+    }
+
+    const restart = () => {
+        for (let i = 0; i < 9; i++){
+            gameBoard.updateBoard(i, "");
+        }
+        gameBoard.renderBoard();
+        displayController.renderMessage("");
+        gameOver = false;
+        currentPlayerIndex = 0;
+    }
+
+    return {start, handleClick, restart}
+
+})();
+
+function checkForWin(board) {
     const winningCombos = [
         [0,1,2],
         [3,4,5],
@@ -56,133 +113,32 @@ const game = (function() {
         [2,4,6]
     ];
 
-
-
-    function startGame(playerOneName, playerTwoName) {
-        playerOne = createPlayer(playerOneName, 'X');
-        playerTwo = createPlayer(playerTwoName, 'O');
-        currentPlayer = playerOne;
-        gameBoard.clearBoard();
-        displayController.renderBoard();
-        isGameOver = false;
-        
-    };
-
-    
-
-    function changeCurrentPlayer() {
-        currentPlayer = (currentPlayer == playerOne) ? playerTwo : playerOne;
-        displayController.playerTurnMessage();
-    };
-
-    const checkForWin = (board, marker) =>{
-        return winningCombos.some(combo =>
-            combo.every(index => board[index] == marker)
-        );
-    };
-
-    const checkForTie = (board) => {
-        return board.every(cell => cell !== null);
-    };
-
-    
-
-    function playGame(index) {
-        if (isGameOver) return;
-
-        
-        
-        if (gameBoard.setCell(index, currentPlayer.marker)){
-            
-            console.log(`${currentPlayer.name}'s turn!`); 
-            gameBoard.setCell(currentPlayer.marker);
-            gameBoard.displayBoard();
-            if (checkForWin(gameBoard.getBoard(), currentPlayer.marker)) {
-                displayController.winnerMessage();
-                setTimeout( () => {
-                    isGameOver = true;
-                    return;
-                }, 1000);                
-            };
-
-            if (checkForTie(gameBoard.getBoard())) {
-                displayController.tieMessage();
-                isGameOver = true;
-                return;
-            }
-            changeCurrentPlayer();
-            displayController.renderBoard();
-        };
-
-    };
-
-    const getPlayerOne = () => playerOne;
-    const getPlayerTwo = () => playerTwo;
-    const getCurrentPlayer = () => currentPlayer;
-
-    return {startGame, playGame, getPlayerOne, getPlayerTwo, getCurrentPlayer, checkForWin}
-
-
-})();
-
-const displayController = ( function () {
-    const gameGrid = document.getElementById("game-grid");
-    const displayMessage = document.getElementById("display-message");
-
-    function renderBoard () {
-        gameGrid.innerHTML = "";
-        gameBoard.getBoard().forEach((cell, index) => {
-            const cellEl = document.createElement('div');
-            cellEl.classList.add('grid-cell');
-            cellEl.setAttribute("id", `cell-${index+1}`)
-            cellEl.innerHTML = '';
-
-            if (cell === "X" || cell === "O"){
-                cellEl.innerHTML = `${cell}`;
-            }
-
-            gameGrid.appendChild(cellEl);
-            cellEl.addEventListener("click", () => {
-                game.playGame(index);
-            });
-
-            
-        });
-
-    };
-
-
-    function playerTurnMessage () {
-        currentPlayer = game.getCurrentPlayer();
-        displayMessage.innerHTML = `${currentPlayer.name}'s turn!`;
+    for (let i = 0; i < winningCombos.length; i++){
+        const [a, b, c] = winningCombos[i];
+        if (board[a] && board[a] == board[b] && board[a] == board[c]){
+            return true;
+        }
     }
+    return false;
+}
 
-    function winnerMessage() {
-        currentPlayer = game.getCurrentPlayer();
-        displayMessage.innerHTML = `${currentPlayer.name} wins!`;
-        console.log(`${currentPlayer.name} wins!`);
-    }
+function checkForTie (board) {
+    return board.every(cell => cell !== "");
+};
 
-    function tieMessage() {
-        displayMessage.innerHTML = "Its a tie!";
-        console.log("Tie");
-    }
+const startBtn = document.getElementById("start-btn");
+startBtn.addEventListener("click", () => {
+    gameController.start();
+    const audioElement = document.getElementById('game-music');
+    const playAudio = () => {
+        audioElement.play();
+        startBtn.removeEventListener('click', playAudio);
+    };
+    playAudio();
+})
 
-    
+const restartBtn = document.getElementById("restart-btn");
+restartBtn.addEventListener("click", () => {
+    gameController.restart();
+})
 
-    return {renderBoard, playerTurnMessage, winnerMessage, tieMessage}
-})();
-
-
-
-document.addEventListener("DOMContentLoaded", (event) => {
-    game.startGame("Player 1", "Player 2");
-
-    // const musicEl = document.getElementById("game-music");
-    // const playMusic = () => {
-    //     musicEl.play();
-    //     document.removeEventListener('click', playMusic);
-    // };
-    // document.addEventListener('click', playMusic);
-
-});
